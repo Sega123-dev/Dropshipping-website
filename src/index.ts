10243733728322e19675609dae7d3f051e55d3cd
Sega123-dev <brajicsergej@gmail.com>
2024-09-29 18:31:27 +0200

    Update

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCVQV958Z41fzCxgG9S11QAvBzW1xZy_4o",
+  authDomain: "dropshiping-website.firebaseapp.com",
+  projectId: "dropshiping-website",
+  storageBucket: "dropshiping-website.appspot.com",
+  messagingSenderId: "88265939594",
+  appId: "1:88265939594:web:2f6292f1ca92a5c3e9eb73",
+  measurementId: "G-E3T06T74CL",
+};
+console.log("Nigger");
